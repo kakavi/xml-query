586070784bf4ceff447c516d653c3b8a519c880e
Victor kakama <vkakama@gmail.com>
2019-05-10 00:53:44 +0300

    ignore root node since sometimes its too dynamic to know

--- src/main/groovy/xmlquery/XQL.groovy
+++ src/main/groovy/xmlquery/XQL.groovy
@@ -19,9 +19,25 @@ class XQL {
 //
     }
 
+/*
     String query() {
         def xml = new XmlParser().parseText(xmlString)
         xml."$rootNode".findAll { p ->
+            if (!p."$conditionNode") return
+            p."${conditionNode}"[0].text().equals(oldValue)
+        }.each { p ->
+            p."${nodeToUpdate}"[0].value = newValue
+        }
+        def stringWriter = new StringWriter()
+        new XmlNodePrinter(new PrintWriter(stringWriter)).print(xml)
+        def newXml = stringWriter.toString()
+        return newXml
+    }
+*/
+
+    String query() {
+        def xml = new XmlParser().parseText(xmlString)
+        xml.depthFirst().findAll { p ->
             if (!p."$conditionNode") return
             p."${conditionNode}"[0].text().equals(oldValue)
         }.each { p ->
